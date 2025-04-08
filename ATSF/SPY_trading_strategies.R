97e23ad00d3e57e6e235dbe846ebec94ee5da9ed
Oscar Echeverry <echevosc@students.zhaw.ch>
2025-04-08 10:36:18 +0200

    nn ready

--- ATSF/SPY_trading_strategies.R
+++ ATSF/SPY_trading_strategies.R
@@ -28,6 +28,7 @@ library(quantmod)
 library(ggplot2)
 library(dplyr)
 
+#################################################################################
 
 # ---- 1️⃣ Lade SPY-Daten von Yahoo Finance ----
 symbol <- "SPY"
@@ -71,6 +72,31 @@ plot(df$Close, col=2, main="Close")
 
 par(mfrow=c(1,1))
 ts.plot(diff(log(df$Close)), col=3, main="Log-returns")
+
+
+###############################################
+# plots in 'final design'
+
+# # ---- 2️⃣ Erstelle SPY-Plot im exakten Stil ----
+# plot_spy <- ggplot(df, aes(x = Date)) +
+#   geom_line(aes(y = Overnight_Strategy, color = "Buy Close, Sell Next Open"), linewidth = 1.2) +
+#   geom_line(aes(y = Intraday_Strategy, color = "Buy Open, Sell Close"), linewidth = 1.2) +
+#   geom_line(aes(y = Buy_Hold_Strategy, color = "Buy and Hold"), linewidth = 1.2) +
+#   scale_color_manual(values = c("Buy Close, Sell Next Open" = "green", 
+#                                 "Buy Open, Sell Close" = "red",
+#                                 "Buy and Hold" = "blue")) +
+#   scale_y_continuous(breaks = seq(0, max(df$Overnight_Strategy, df$Intraday_Strategy, df$Buy_Hold_Strategy, na.rm = TRUE), by = 100)) +  # Y-Achse in 100er-Schritten
+#   scale_x_date(date_breaks = "2 years", date_labels = "%Y") +  # Zweijährliche Intervalle für bessere Lesbarkeit
+#   labs(title = "S&P 500 (SPY) After Hours vs. Regular Trading vs. Buy-and-Hold Performance",
+#        x = "Date",
+#        y = "Cumulative Return (%)",
+#        color = "") +  # Keine Legendenüberschrift
+#   theme_minimal() +
+#   theme(legend.position = "top",
+#         plot.title = element_text(face = "bold", size = 14),
+#         axis.text = element_text(size = 12),
+#         axis.title = element_text(size = 13),
+#         axis.text.x = element_text(angle = 45, hjust = 1))  # Dreht die X-Achsenbeschriftungen für bessere Lesbarkeit
 
 ########################################################################################-----------------------------
 # Fit a GARCH to log-returns of 'Close'
@@ -462,13 +488,173 @@ sharpe_nn<-sqrt(365)*mean(perf_nn,na.rm=T)/sqrt(var(perf_nn,na.rm=T))
 par(mfrow=c(1,1))
 plot(cumsum(perf_nn),main=paste("NN cumulated performances out-of-sample, sharpe=",round(sharpe_nn,2),sep=""))
 
-###############################################################################3
-# repeat the nn fit with a bigger nn c(20,10)
+###############################################################################
 
+
+
+
+# overnigth-returns strategy
+
+# Erstelle Dataframe mit relevanten Spalten
+symbol <- "SPY"
+getSymbols(symbol, src = "yahoo", from = "1993-01-01", to = Sys.Date(), auto.assign = TRUE)
+df <- get(symbol)
+
+df <- data.frame(Date = index(df),
+                 Open = as.numeric(Op(df)),
+                 Close = as.numeric(Cl(df)))
+
+# Berechne Renditen für alle drei Strategien
+df <- df %>%
+  mutate(Open_log = log(Open),
+         Close_log = log(Close)) %>%
+  mutate(Overnight_log_Return = Open_log - lag(Close_log),# Rendite: Buy Close, Sell Next Open
+         Intraday_log_Return = Close_log - Open_log,# Rendite: Buy Open, Sell Close
+         Daily_log_Return = Close_log - lag(Close_log)) # Rendite: Buy and Hold (tägliche Rendite)
+
+len_before <- dim(df)[1]
+
+# Entferne NA-Werte
+df <- na.omit(df)
+
+len_after <- dim(df)[1]
+
+# number of removed rows
+len_before - len_after
+
+head(df)
+
+
+
+###############################################
+
+# Fit a GARCH to log-returns of 'Overnight_log_Return'
+
+
+x_fit<-as.ts(na.omit(df$Overnight_log_Return))
+# GARCH(1,1)
+y.garch_11<-garchFit(~garch(1,1),data=x_fit,delta=2,include.delta=F,include.mean=F,trace=F)
+
+ts.plot(x_fit)
+lines(y.garch_11@sigma.t,col="red")
+#-----------------
+
+standard_residuals<-y.garch_11@residuals/y.garch_11@sigma.t
+ts.plot(standard_residuals)
+#-----------------
+
+par(mfrow=c(2,1))
+acf(x_fit,main="Acf log-returns",ylim=c(0,0.1))
+acf(standard_residuals,main="Acf standardized residuals GARCH(1,1)",ylim=c(0,0.1))
+
+
+# acf and residuals acf sugest a dependency on lag 13, also on lag 29?
+
+# Specify target and explanatory data: we use first 13 lags based on above data analysis
+
+
+
+# convert data to xts
+
+spy_xts <- xts(df[, -1], order.by = df$Date)
+dim(spy_xts)
+x<-na.omit(spy_xts$Overnight_log_Return)
+head(x)
+
+data_mat <- cbind(
+  x,
+  stats::lag(x, -1),
+  stats::lag(x, -2),
+  stats::lag(x, -3),
+  stats::lag(x, -4),
+  stats::lag(x, -5),
+  stats::lag(x, -6),
+  stats::lag(x, -7),
+  stats::lag(x, -8),
+  stats::lag(x, -9),
+  stats::lag(x, -10),
+  stats::lag(x, -11),
+  stats::lag(x, -12),
+  stats::lag(x, -13)
+)
+
+head(data_mat)
+
+
+# Check length of time series before na.exclude
+dim(data_mat)
+data_mat<-na.exclude(data_mat)
+# Check length of time series after removal of NAs
+dim(data_mat)
+head(data_mat)
+tail(data_mat)
+
+# Specify in- and out-of-sample episodes
+in_out_sample_separator <- index(data_mat)[round(dim(data_mat)[1]*0.8)] # "2018-10-08"
+in_out_sample_separator
+
+target_in<-data_mat[paste("/",in_out_sample_separator,sep=""),1]
+tail(target_in)
+explanatory_in<-data_mat[paste("/",in_out_sample_separator,sep=""),2:ncol(data_mat)]
+tail(explanatory_in)
+
+target_out<-data_mat[paste(in_out_sample_separator,"/",sep=""),1]
+head(target_out)
+tail(target_out)
+explanatory_out<-data_mat[paste(in_out_sample_separator,"/",sep=""),2:ncol(data_mat)]
+
+
+train<-cbind(target_in,explanatory_in)
+test<-cbind(target_out,explanatory_out)
+head(test)
+tail(test)
+nrow(train)
+nrow(test)
+
+#-------------------------------------------------------------------------------
+#  Prepare the datasets
+
+# Scaling data for the NN
+
+maxs <- apply(data_mat, 2, max) 
+mins <- apply(data_mat, 2, min)
+# Transform data into [0,1]  
+scaled <- scale(data_mat, center = mins, scale = maxs - mins)
+
+apply(scaled,2,min)
+apply(scaled,2,max)
+#-----------------
+
+# Train-test split
+train_set <- scaled[paste("/",in_out_sample_separator,sep=""),]
+test_set <- scaled[paste(in_out_sample_separator,"/",sep=""),]
+
+train_set<-as.matrix(train_set)
+test_set<-as.matrix(test_set)
+#-----------------------------------
+
+colnames(train_set)<-paste("lag",0:(ncol(train_set)-1),sep="")
+n <- colnames(train_set)
+#  
+f <- as.formula(paste("lag0 ~", paste(n[!n %in% "lag0"], collapse = " + ")))
+
+tail(train_set)
+
+
+##################################################################################################
+# fit a bigg enough NN -  c(20,10)
 
 # Set/fix the random seed 
 set.seed(4)
-nn <- neuralnet(f,data=train_set,hidden=c(20,10),linear.output=F)  # ca. 15 min training
+nn <- neuralnet(f,data=train_set,hidden=c(20,10),linear.output=F)  # ca. 1 std training
+
+
+setwd("C:/Users/Oscar/OneDrive - ZHAW/s - FS 2025/ATSF/Project/ATSP_project")
+save(nn, file = "nn_model_20_10.RData")
+
+# to load
+# load("nn_model_20_10.RData")
+
 
 #------------------------------------
 # plot(nn)
@@ -479,9 +665,10 @@ MSE.in.nn<-mean(((train_set[,1]-nn$net.result[[1]])*(max(data_mat[,1])-min(data_
 MSE.in.nn
 # 2. With re-scaling: MSE based on scale of original data
 scaling_term<-(max(data_mat[,1])-min(data_mat[,1]))
-MSE.in.nn<-mean(((train_set[,1]-nn$net.result[[1]])*scaling_term)^2)
+MSE.in.nn_scaling_term<-mean(((train_set[,1]-nn$net.result[[1]])*scaling_term)^2)
 
 
+##################################################
 # Out-of-sample performance
 # 1. Compute out-of-sample predictions based on transformed data
 # Provide test-data to predict: use explanatory columns 2:ncol(test_set) (First column is forecast target)
@@ -489,11 +676,13 @@ pr.nn <- predict(nn,test_set[,2:ncol(test_set)])
 predicted_scaled<-pr.nn
 # Numbers are between 0 and 1
 tail(predicted_scaled)
+
 # Transform forecasts back to original data: rescale and shift by min(data_mat[,1])
 predicted_nn <- predicted_scaled*scaling_term+min(data_mat[,1])
+
 test.r <- test_set[,1]*scaling_term+min(data_mat[,1])
 # Check: test.r is the same as test[,1]
-test[,1]-test.r
+# test[,1]-test.r
 # Calculating MSE
 MSE.out.nn <- sum((test.r - predicted_nn)^2)/nrow(test_set)
 
